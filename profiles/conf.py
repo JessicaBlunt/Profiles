e5a4d8a33014643ca7d6e3ba6bb61552e2a52300
JessicaBlunt <jessica.m.blunt-1@ou.edu>
2020-03-10 12:18:05 -0500

    read sensor serial numbers from Azure; include QC flags in NetCDF files

--- profiles/conf.py
+++ profiles/conf.py
@@ -5,8 +5,8 @@ from types import SimpleNamespace
 coef_info = SimpleNamespace(USE_AZURE=None, AZURE_CONNECTION_STRING=None, FILE_PATH=None)
 # If you do not use Azure, you MUST use a local coefs folder. See oucass.github.io/Profiles/coefs.html for instructions 
 coef_info.USE_AZURE="YES"
-# If you are using Azure, retrieve the connection strings from your portal. When one key is being regenerated, the other will be used
-coef_info.AZURE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=stmesonet001;AccountKey=Jw4Cw7uuE2FljqLG2P7zPHYoDluAv5Xd/Y0cHWItiYGJlSJno2mLJsXlFTXTuCdq+gpcz68BR9+BryTmhphWBw==;EndpointSuffix=core.windows.net"
+# If you are using Azure, retrieve the connection strings from your portal.
+coef_info.AZURE_CONNECTION_STRING=""
 # If you are NOT using Azure, put the path to the coefs folder here
 coef_info.FILE_PATH=""
 
